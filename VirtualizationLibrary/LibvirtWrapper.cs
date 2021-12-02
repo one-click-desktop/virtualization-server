6c99bc44e2e342bb7155c9901638ecfd74c30222
smogork <krzysztof.smogor@wp.pl>
2021-12-02 19:49:57 +0100

    Utworzenie zarysu biblioteki do zarzadzania maszynami wirtualnymi

---
+++ VirtualizationLibrary/LibvirtWrapper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OneClickDesktop.VirtualizationLibrary
+{
+    /// <summary>
+    /// Klasa zajmuje się komunikacją z libvirtem i udostepnia interfejs dla VirtualizationManagera.
+    /// </summary>
+    public class LibvirtWrapper
+    {
+        
+    }
+}
---
+++ VirtualizationLibrary/VagrantWrapper.cs
@@ -0,0 +1,11 @@
+namespace OneClickDesktop.VirtualizationLibrary
+{
+    /// <summary>
+    /// Klasa zajmuje się wykonywaniem polecen na specjalnie przygotowanym vagrantfile'u
+    /// i udostepnia interfejs dla VirtualizationManagera.
+    /// </summary>
+    public class VagrantWrapper
+    {
+        
+    }
+}
