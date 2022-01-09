dd4521e5a9f73a017553dd80c11dfaa73d4354d6
VenMolom <venmolom@protonmail.com>
2022-01-09 20:18:19 +0100

    build

--- build.sh
+++ build.sh
@@ -10,3 +10,4 @@ docker build . -t one-click-desktop/virtualization-server
 # 2. Attaching volume /var/run/libvirt/libvirt_sock:/var/run/libvirt/libvirt_sock
 # 3. Attaching volume /var/lib/libvirt/:/var/lib/libvirt/
 # 4. Attaching /root/.vagrant.d/boxes/ from container to local machine executive user's ~/.vagrant.d/boxes/
+# 5. Attaching ./VirtualizationServer/config to /app
